f53a385f98dc94952f64893ae6591ed6ce68bf99
noelbytes <allan34291@gmail.com>
2023-08-27 01:24:16 +0530

    Updated the comment

--- src/main/java/com/telusko/quizapp/controller/QuizController.java
+++ src/main/java/com/telusko/quizapp/controller/QuizController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-// controller for quiz
+// controller for quiz - update
 @RestController
 @RequestMapping("quiz")
 public class QuizController {
