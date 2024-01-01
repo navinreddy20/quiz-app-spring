59301171ad0425a072695edbec28cfca9e2419be
akhil <akhilboyanapalli657@gmail.com>
2023-12-31 22:35:21 -0600

    comments added

--- src/main/java/com/telusko/quizapp/controller/QuestionController.java
+++ src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+//controller for question
 
 @RestController
 @RequestMapping("question")
